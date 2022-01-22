766f094fe5679d017b4ba91c70a84b4b4f80afc5
Tuan Ngo <ngotuan911@gmail.com>
2022-01-21 20:22:19 -0800

    Basic to do list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.scss';
+import { useState } from 'react';
 function App() {
+  const [jobs, setJobs] = useState([]);
+  const [job, setJob] = useState('');
+
+  const handleClick = ()=> {
+    setJobs(prev => [...prev,job]);
+    setJob('');
+    document.querySelector('.listInput').focus();
+  }
+  const handleDelete = (parameter) => {
+    const filter = jobs.filter(job => job !== parameter)
+    setJobs(filter);
+    document.querySelector('.listInput').focus();
+  }
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input className="listInput" value={job}  onChange={e => setJob(e.target.value)} />
+      <button onClick={handleClick}>Add</button>
+      <div >
+        <ul className="listContainer">
+        {jobs.map((job, index) => (
+          <li key={index} >{job} <button job={job} onClick={ () => handleDelete(job)} style={{marginLeft : 15}}>Delete</button></li>
+
+          ))
+        }
+      </ul>
+      </div>
+
     </div>
   );
 }
